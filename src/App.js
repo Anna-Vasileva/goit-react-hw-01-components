d383d87329d3b32cdd2afc984146a9fe478a3e09
Anna Vasileva <anette.vasileva@gmail.com>
2021-10-20 15:50:11 +0300

    Add FriendList component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Profile from "./components/Profile";
 import user from "./data/user.json";
 import Statistics from "./components/Statistics";
 import statisticalData from "./data/statistical-data.json";
+import FriendList from "./components/FriendList";
+import friends from "./data/friends.json";
 // import { Container } from "./components/Container";
 // import './App.css';
 
@@ -17,6 +19,7 @@ function App() {
       />
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
     </>
   );
 }
